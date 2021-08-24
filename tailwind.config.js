c41c1ffd3d28857e6bbe2ad23c2a0ac4905b4583
Pablo Garcia Murillo <pablo.ismael90@gmail.com>
2021-08-24 15:54:34 -0600

    agregando mas diseno

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+        backgroundColor: ['active'],
+        outline: ['hover'],
+        shadowoutline: ['hover'],
+    }
   },
   plugins: [],
 }
